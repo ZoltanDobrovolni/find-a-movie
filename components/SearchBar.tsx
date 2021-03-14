590719c3144b9e3e5fbf2a38d385db21ccd4a9cb
Zoltan Dobrovolni <zoltan.dobrovolni@kuka.com>
2021-03-14 12:51:34 +0100

    Add loading spinner to input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import { Paper, IconButton, InputBase } from '@material-ui/core';
+import { Paper, IconButton, InputBase, CircularProgress, Box } from '@material-ui/core';
 import { useQuery } from '@apollo/client';
 import useStyles from '../styles/styles';
 import { Movie, MoviesSearchQueryResult, SearchResult } from '../types/types';
@@ -17,7 +17,7 @@ type SearchBarProps = {
 
 const SearchBar : FC<SearchBarProps> = ({ handleSearchResultChange }) => {
     const [searchInputValue, setSearchInputValue] = useState<string>('');
-    const [searchValue, setSearchValue] = useState<string>('');
+    const [searchValue, setSearchValue] = useState<string | null>(null);
     const classes = useStyles();
 
     const { loading, error, data } = useQuery<MoviesSearchQueryResult, MoviesSearchQueryVars>(
@@ -27,11 +27,14 @@ const SearchBar : FC<SearchBarProps> = ({ handleSearchResultChange }) => {
 
     useEffect(() => {
         // console.error(error);
+        if (loading) {
+
+        }
         if (data) {
             console.log(data.searchMovies)
             handleSearchResultChange(data.searchMovies);
         }
-    }, [data, handleSearchResultChange, error]);
+    }, [data, handleSearchResultChange, error, loading]);
 
     const handleClick: SearchButtonEventHandler = async () => {
         setSearchValue(searchInputValue);
@@ -45,17 +48,24 @@ const SearchBar : FC<SearchBarProps> = ({ handleSearchResultChange }) => {
     const handleSearchInputChange: InputChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => setSearchInputValue(event.target.value);
 
     return (
-        <Paper component="form" onSubmit={handleSubmit} className={classes.root}>
+        <Paper component="form" onSubmit={handleSubmit} className={classes.searchPaper}>
             <InputBase
                 className={classes.input}
                 placeholder="Search Your Movie"
                 inputProps={{ 'aria-label': 'search google maps' }}
+                disabled={loading}
                 value={searchInputValue}
                 onChange={handleSearchInputChange}
             />
-            <IconButton onClick={handleClick} className={classes.iconButton} aria-label="search">
-                <SearchIcon />
-            </IconButton>
+            {loading && searchValue ?
+                <Box component="span" m={1} className={classes.searchBarSpinner}>
+                    <CircularProgress size="1rem" />
+                </Box>
+                :
+                <IconButton onClick={handleClick} disabled={loading} className={classes.iconButton} aria-label="search">
+                    <SearchIcon/>
+                </IconButton>
+            }
         </Paper>
     );
 }
--- styles/styles.ts
+++ styles/styles.ts
@@ -1,30 +1,41 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    padding: {
-      padding: theme.spacing(1),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
-    iconButton: {
-      padding: 10,
-    },
-  }),
+    createStyles({ // todo egésznek utánaolvasni, hogy kell jól csinálni
+        searchPaper: {
+            display: 'flex',
+            alignItems: 'center',
+            height: '3rem',
+        },
+        margin: {
+            margin: theme.spacing(2),
+        },
+        padding: {
+            padding: theme.spacing(2),
+        },
+        paper: {
+            padding: theme.spacing(2),
+            // textAlign: 'center',
+            // color: theme.palette.text.secondary, // todo
+            width: '60ch',
+        },
+        overflowEllipsis: {
+            overflow: 'hidden',
+            // maxHeight: '10rem',
+            // whiteSpace: 'nowrap',
+            // textOverflow: 'ellipsis',
+        },
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1, // todo
+        },
+        iconButton: {
+            padding: 10,
+        },
+        searchBarSpinner: {
+            marginRight: '0.9rem'
+        }
+    }),
 );
 
 export default useStyles;
